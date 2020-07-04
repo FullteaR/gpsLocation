03f76e6bda0857e803e5124fda2f63949545ec00
Rikuta Furuta <rikuta@furutan.com>
2020-07-04 18:25:20 +0900

    go

---
+++ db/sql/initialize.sql
@@ -0,0 +1,14 @@
+DROP SCHEMA IF EXISTS location;
+CREATE SCHEMA location;
+DROP SCHEMA IF EXISTS event;
+CREATE SCHEMA event;
+USE location;
+
+DROP TABLE IF EXISTS gps;
+CREATE TABLE gps(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  event_id INT NOT NULL,
+  date DATETIME,
+  latitude FLOAT NOT NULL,
+  longitude FLOAT NOT NULL
+)
--- mysql/sqls/initialize.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE races;
-use races;
